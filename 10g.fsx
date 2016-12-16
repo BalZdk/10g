beff326037376051ef58da96bfd17d5c3d839c0a
Balder I <balderi@tsponline.dk>
2016-12-16 11:11:03 +0100

    class stuff

--- 10g.fsx
+++ 10g.fsx
@@ -1,2 +1,23 @@
-test
-hest
+let rng = new System.Random()
+
+type animal(Weight : float, MaxSpeed : float) =
+    let mutable Speed = 0.0
+    let mutable NeededFood = 0.0
+
+    member this.weight = Weight
+    member this.speed
+        with get () = Speed
+        and set (value) = Speed <- value
+    member this.maxSpeed = MaxSpeed
+    member this.neededFood
+        with get () = NeededFood
+        and set (value) = NeededFood <- value
+
+    member setCurrentSpeed()
+
+    member setNeededFood()
+
+
+    new (MaxSpeed) =
+        let weight = float(rng.Next(70, 301))
+        animal(weight, MaxSpeed)
